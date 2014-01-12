a8f7b54b400398cb3dc658822da5dcdde8b6297e
Arthur Alvim <arth.alvim@gmail.com>
2014-01-12 15:16:01 -0300

    Adicionando dicionarios para o spell checking.
    1 - Atualizando o Preferences.sublime-settings com novos parametros.
    2 - Movendo arquivos para raiz do sublime-preferences.
    3 - Atualizando os pacotes do ubuntu.media.packages.

--- sublime-preferences/extra/Latex/RunBuild.py
+++
@@ -1,8 +0,0 @@
-import sublime
-import sublime_plugin
-
-
-class RunBuildCommand(sublime_plugin.WindowCommand):
-    def run(self, build_system):
-        self.window.run_command("set_build_system", {"file": build_system})
-        self.window.run_command("build")
