8941f5099d82bea47a1d2fa5b50ba03fef79233d
Arthur Alvim <afmalvim@gmail.com>
2015-10-01 15:33:34 -0300

    Atualizando scripts de setup do osx.

--- setup-osx.sh
+++ setup-osx.sh
@@ -1,4 +1,4 @@
-# dotfiles setup-mac
+# dotfiles setup-osx
 
 # check if brew is installed
 
@@ -20,13 +20,37 @@ cd HOME_DIR
 DOTFILES_DIR="$HOME_DIR/dotfiles"
 
 if [ ! -d "$DOTFILES_DIR" ]; then
-	git clone git@github.com:arthuralvim/dotfiles.git $DOTFILES_DIR
+    git clone git@github.com:arthuralvim/dotfiles.git $DOTFILES_DIR
 fi
 
 cd $DOTFILES_DIR
+
+for file in $DOTFILES_DIR/.{aliases, bash_profile, bashrc, cookiecutterrc, exports, extra, functions, gitconfig, gitignore, global_gitignore, vimrc, zshrc}
+do
+    echo "pwd/.$file"
+    # ln -sf "pwd/.$file" "$HOME_DIR/.$file"
+done
+
+# zsh and ohmyzsh
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s /bin/zsh
+
+sudo chown $USER .pip
 
 # install brew packages
 
 brew tap homebrew/bundle
 brew bundle install --file=Brewfile
 brew bundle install --file=Caskfile
+
+# install vim settings
+
+cd $HOME_DIR
+vim +NeoBundleInstall +qall
+
+# install sublime settings
+
+rsync -vazh --partial $DOTFILES_DIR/sublime-preferences/ ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+
+
