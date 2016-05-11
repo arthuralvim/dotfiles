eb43b0cd7c3f6802e53b402b0a876956bd19960e
Arthur Alvim <afmalvim@gmail.com>
2016-05-11 00:19:36 -0300

    Atualizando os scripts de instalação do git.

--- common/functions.sh
+++ common/functions.sh
@@ -11,13 +11,19 @@
 #  36  # Cyan    #
 #  37  # White   #
 ##################
-function print { echo  "\033[1;32m=> $1\033[0m"; }
-function msg_checking { echo  "\033[1;32m=> $1 ✔\033[0m"; }
-function msg_installing { echo  "\033[1;33m==> $1 [updating] ✔\033[0m"; }
-function msg_install { echo  "\033[1;33m==> $1 [installing]: $ $2\033[0m"; }
-function msg_ok { echo  "\033[1;32m==> $1 installed ✔\033[0m"; }
-function msg { echo  "\033[0;32m$1\033[0m"; }
-function msg_alert { echo "\033[1;31m✖ $1 ✖\033[0m"; }
+
+export OSX=$(test "`uname`" == "Darwin" && echo "osx")
+export LINUX=$(test "`uname`" == "Linux" && echo "linux")
+export DOTFILES_DIR="$HOME/Dropbox/dotfiles"
+
+function print { echo -e "\033[1;32m=> $1\033[0m"; }
+function msg_checking { echo -e "\033[1;32m=> $1 ✔\033[0m"; }
+function msg_installing { echo -e "\033[1;33m==> $1 [updating] ✔\033[0m"; }
+function msg_install { echo -e "\033[1;33m==> $1 [installing]: $ $2\033[0m"; }
+function msg_ok { echo -e "\033[1;32m==> $1 installed ✔\033[0m"; }
+function msg { echo -e "\033[0;32m$1\033[0m"; }
+function msg_alert { echo -e "\033[1;31m✖ $1 ✖\033[0m"; }
+
 function file_to_array() {
     array=()
     while IFS= read -r line
--- version-control/git/install.sh
+++ version-control/git/install.sh
@@ -1,14 +1,37 @@
+#!/bin/bash
+
 source common/functions.sh
 
 # git
 if which git &> /dev/null; then
     msg_checking "git"
 else
-    msg_install "git" "brew install git"
-    brew install git
-    brew install git-extras
-    brew install git-cal
-    brew install git-flow
-    brew install meld
-    msg_ok "OK"
+    if [ "$LINUX" = "linux" ]; then
+        msg_install "git" "apt-get install git git-core"
+        msg_ok "OK"
+    fi
+
+    if [ "$OSX" = "osx" ]; then
+        msg_install "git" "brew install git"
+        brew install git
+        brew install git-extras
+        brew install git-cal
+        brew install git-flow
+        brew install meld
+        msg_ok "OK"
+    fi
+
+fi
+
+# .gitconfig
+
+if [ -f "version-control/git/.gitconfig" ]; then
+    msg_installing ".gitconfig"
+    ln -s $DOTFILES_DIR/version-control/git/.gitconfig $HOME/.gitconfig
+    msg_installing ".git-commit-template"
+    git config --global commit.template "${DOTFILES_DIR}/version-control/git/git-commit-template.txt"
+    msg_installing ".git-init-template"
+    git config --global init.templatedir "${DOTFILES_DIR}/version-control/git/init_template"
+    msg_installing ".global-gitignore"
+    git config --global core.excludesfile "${DOTFILES_DIR}/version-control/git/.global_gitignore"
 fi
